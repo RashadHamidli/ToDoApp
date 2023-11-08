bd97105dc2627125be36c167f776ce89427a6586
mr_rashad <mr_rashad@email.com>
2023-11-08 20:42:24 +0400

    add

--- src/main/java/com/company/reposiroty/TaskRepository.java
+++ src/main/java/com/company/reposiroty/TaskRepository.java
@@ -1,7 +1,9 @@
 package com.company.reposiroty;
 
+import com.company.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TaskRepository {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
--- src/main/java/com/company/reposiroty/UserRepository.java
+++ src/main/java/com/company/reposiroty/UserRepository.java
@@ -1,7 +1,9 @@
 package com.company.reposiroty;
 
+import com.company.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User,Long> {
 }
