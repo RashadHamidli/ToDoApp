34d5fc4ec7d0e7c019116cea89ededd5f044768b
mr_rashad <mr_rashad@email.com>
2023-11-08 19:48:35 +0400

    add

--- src/main/java/com/company/Launcher.java
+++ src/main/java/com/company/Launcher.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToDoAppApplication {
+public class Launcher {
 
     public static void main(String[] args) {
-        SpringApplication.run(ToDoAppApplication.class, args);
+        SpringApplication.run(Launcher.class, args);
     }
 
 }
