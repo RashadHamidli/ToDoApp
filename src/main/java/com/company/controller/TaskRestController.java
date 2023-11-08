97bc7461b303084392145c093b9a9d9ff61faeea
mr_rashad <mr_rashad@email.com>
2023-11-08 21:00:13 +0400

    add

--- src/main/java/com/company/controller/TaskRestController.java
+++ src/main/java/com/company/controller/TaskRestController.java
@@ -1,5 +1,6 @@
 package com.company.controller;
 
+import com.company.dto.TaskDTO;
 import com.company.dto.UserDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,5 +11,34 @@ import java.util.List;
 @RequestMapping("/tasks")
 public class TaskRestController {
 
+    @GetMapping
+    public List<TaskDTO> getAllTasks() {
+        // Tüm görevleri getirme işlemi
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{taskId}")
+    public TaskDTO getTaskById(@PathVariable Long taskId) {
+        // Belirli bir görevi ID'ye göre getirme işlemi
+        return taskService.getTaskById(taskId);
+    }
+
+    @PostMapping("/user/{userId}")
+    public TaskDTO createTaskForUser(@PathVariable Long userId, @RequestBody TaskDTO taskDTO) {
+        // Belirli bir kullanıcı için yeni bir görev oluşturma işlemi
+        return taskService.createTaskForUser(userId, taskDTO);
+    }
+
+    @PutMapping("/{taskId}")
+    public TaskDTO updateTask(@PathVariable Long taskId, @RequestBody TaskDTO taskDTO) {
+        // Görev güncelleme işlemi
+        return taskService.updateTask(taskId, taskDTO);
+    }
+
+    @DeleteMapping("/{taskId}")
+    public void deleteTask(@PathVariable Long taskId) {
+        // Görev silme işlemi
+        taskService.deleteTask(taskId);
+    }
 
 }
