c56bbb78e5b957c79819c2aa2154f176f1b4c7f7
mr_rashad <mr_rashad@email.com>
2023-11-10 14:43:50 +0400

    add

---
+++ src/main/java/com/company/dto/respons/JwtAuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.company.dto.respons;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthenticationResponse {
+    private String token;
+}
