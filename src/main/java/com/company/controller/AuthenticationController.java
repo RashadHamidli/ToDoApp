eb375eed1f6aa8257a4d83a7b394a0b0bc26a8e8
mr_rashad <mr_rashad@email.com>
2023-11-10 14:42:40 +0400

    add

---
+++ src/main/java/com/company/controller/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.company.controller;
+
+import com.company.dao.request.SignUpRequest;
+import com.company.dao.request.SigninRequest;
+import com.company.dao.response.JwtAuthenticationResponse;
+import com.company.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+    @PostMapping("/signup")
+    public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request) {
+        return ResponseEntity.ok(authenticationService.signup(request));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SigninRequest request) {
+        return ResponseEntity.ok(authenticationService.signin(request));
+    }
+}
